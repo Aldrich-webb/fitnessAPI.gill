d9c45a099e6d09a7036a151be5f7b34cde7c1a87
aldrichgillwork25 <aldrichgillwork25@gmail.com>
2025-08-13 23:39:17 +0800

    edit corsOptions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-    origin:['http://localhost:8000'],
+    origin:['https://fitness-app-client-beta-ivory.vercel.app'],
     credentials: true, 
     optionsSuccessStatus: 200
 };
